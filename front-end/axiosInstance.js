53914f6300444425caa28cdcd3465e0db9cc287e
khalalon <khalilkossentini69@gmail.com>
2025-03-12 14:43:18 +0100

    fix search bar issues

--- front-end/axiosInstance.js
+++ front-end/axiosInstance.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // Set up the base URL for your API
 const axiosInstance = axios.create
   ({
-    baseURL: 'x', // Add protocol  timeout: 5000, 
+    baseURL: '192.168.1.67:5000', // Add protocol  timeout: 5000, 
 });
 
 export default axiosInstance;
