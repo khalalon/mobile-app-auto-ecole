f2a2041219e93035813f98cdd64eacc9e07ca546
khalil <khalilkossentini69@gmail.com>
2025-03-18 22:11:05 +0100

    feat : linking the infrastructure with the front end

--- front-end/axiosInstance.js
+++ front-end/axiosInstance.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 
 // Set up the base URL for your API
 const axiosInstance = axios.create({
-  baseURL: 'http://192.168.1.67:5000', // Added the protocol here
+  baseURL: 'https://8hvhwlbw6d.execute-api.us-east-1.amazonaws.com/prod', 
+  headers: {
+    "Content-Type": "application/json",
+  },
   timeout: 5000, 
 });
 
